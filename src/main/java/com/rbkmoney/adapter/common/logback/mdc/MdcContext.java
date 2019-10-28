624fafa61fd1d59b0f2522c200ec47d47b4358b3
MaNa <malkoas@ya.ru>
2019-10-28 12:03:59 +0300

    update MdcContext, add ServerHandlerMdcDecorator

--- src/main/java/com/rbkmoney/adapter/common/logback/mdc/MdcContext.java
+++ src/main/java/com/rbkmoney/adapter/common/logback/mdc/MdcContext.java
@@ -1,27 +1,22 @@
 package com.rbkmoney.adapter.common.logback.mdc;
 
-import com.rbkmoney.adapter.common.utils.converter.PaymentDataConverter;
 import com.rbkmoney.damsel.domain.TransactionInfo;
 import com.rbkmoney.damsel.proxy_provider.PaymentContext;
 import com.rbkmoney.damsel.proxy_provider.RecurrentTokenContext;
 import org.slf4j.MDC;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class MdcContext {
 
-    private static final String AMOUNT_KEY = "amount";
     public static void mdcPutContext(RecurrentTokenContext context, String[] fieldsToPutInMdc) {
         TransactionInfo transactionInfo = context.getTokenInfo().getTrx();
-        String amount = PaymentDataConverter.getFormattedAmount(context.getTokenInfo().getPaymentTool().getMinimalPaymentCost().getAmount()).toString();
-        MDC.put(AMOUNT_KEY, amount);
         mdcPutContextTransactionInfo(transactionInfo, fieldsToPutInMdc);
     }
 
     public static void mdcPutContext(PaymentContext context, String[] fieldsToPutInMdc) {
         TransactionInfo transactionInfo = context.getPaymentInfo().getPayment().getTrx();
-        String amount = PaymentDataConverter.getFormattedAmount(context.getPaymentInfo().getPayment().getCost().getAmount()).toString();
-        MDC.put(AMOUNT_KEY, amount);
         mdcPutContextTransactionInfo(transactionInfo, fieldsToPutInMdc);
     }
 
@@ -34,8 +29,30 @@ public class MdcContext {
         }
     }
 
+    public static void mdcPutContext(PaymentContext context) {
+        TransactionInfo transactionInfo = context.getPaymentInfo().getPayment().getTrx();
+        mdcPutContextTransactionInfo(transactionInfo);
+    }
+
+    public static void mdcPutContext(RecurrentTokenContext context) {
+        TransactionInfo transactionInfo = context.getTokenInfo().getTrx();
+        mdcPutContextTransactionInfo(transactionInfo);
+    }
+
+    public static void mdcPutContextTransactionInfo(TransactionInfo transactionInfo) {
+        if (transactionInfo != null) {
+            Map<String, String> trxextra = transactionInfo.getExtra();
+            String maskPattern = "\\b\\d{6}([\\d\\s]{2,9})\\d{4}\\b";
+            Pattern pattern = Pattern.compile(maskPattern);
+            for (Map.Entry<String, String> extra : trxextra.entrySet()) {
+                if (pattern.matcher(extra.getValue()).find()) {
+                    MDC.put(extra.getKey(), extra.getValue());
+                }
+            }
+        }
+    }
+
     public static void mdcRemoveContext(String[] fieldsToPutInMdc) {
-        MDC.remove(AMOUNT_KEY);
         for (String field : fieldsToPutInMdc) {
             MDC.remove(field);
         }
