330f70d4edf7be7dd566a1292dbf8ce328124ed0
inalarsanukaev <inalarsanukaev@nb0031.local>
2020-01-21 18:11:52 +0300

    Improve test

--- src/test/java/com/rbkmoney/adapter/common/model/AdapterContextTest.java
+++ src/test/java/com/rbkmoney/adapter/common/model/AdapterContextTest.java
@@ -2,8 +2,10 @@ package com.rbkmoney.adapter.common.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rbkmoney.adapter.common.mapper.SimpleObjectMapper;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.time.Instant;
 
 import static org.junit.Assert.*;
@@ -11,8 +13,8 @@ import static org.junit.Assert.*;
 public class AdapterContextTest {
 
     @Test
-    public void testUnwrapped() throws JsonProcessingException {
-        ObjectMapper om = new ObjectMapper();
+    public void testUnwrapped() throws IOException {
+        ObjectMapper om = new SimpleObjectMapper().createSimpleObjectMapperFactory();
         AdapterContext ac = new AdapterContext();
         ac.setMd("kek");
         PollingInfo pollingInfo = new PollingInfo();
@@ -20,6 +22,10 @@ public class AdapterContextTest {
         ac.setPollingInfo(pollingInfo);
         String str = om.writeValueAsString(ac);
         assertTrue(str.startsWith("{\"md\":\"kek\",\"max_date_time_polling\":"));
+        AdapterContext acRestored = om.readValue(str, AdapterContext.class);
+        assertEquals(ac.getMd(), acRestored.getMd());
+        assertNotNull(acRestored.getPollingInfo());
+        assertEquals(ac.getPollingInfo().getMaxDateTimePolling(), acRestored.getPollingInfo().getMaxDateTimePolling());
     }
 
 }
