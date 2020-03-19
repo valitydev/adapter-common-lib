d10bee3696d81ed449201e49136113f604f585d8
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2020-03-19 17:11:09 +0300

    PROX-396: Redecorate BankCardUtils (#24)

---
+++ src/test/java/com/rbkmoney/adapter/common/utils/converter/BankCardUtilsTest.java
@@ -0,0 +1,57 @@
+package com.rbkmoney.adapter.common.utils.converter;
+
+import com.rbkmoney.damsel.cds.CardData;
+import com.rbkmoney.damsel.cds.ExpDate;
+import com.rbkmoney.damsel.domain.BankCard;
+import com.rbkmoney.damsel.domain.BankCardExpDate;
+import com.rbkmoney.java.cds.utils.model.CardDataProxyModel;
+import org.junit.Test;
+
+import static com.rbkmoney.adapter.common.utils.converter.BankCardUtils.*;
+import static org.junit.Assert.assertEquals;
+
+public class BankCardUtilsTest {
+
+    private static final short TEST_YEAR = 2020;
+    private static final byte TEST_MONTH = 3;
+    private static final byte TEST_NEW_MONTH = 6;
+    private static final ExpDate TEST_EXP_DATE = new ExpDate(TEST_MONTH, TEST_YEAR);
+    private static final CardData TEST_CARD_DATA = new CardData("pan", new ExpDate(TEST_NEW_MONTH, TEST_YEAR));
+    private static final BankCardExpDate TEST_BANK_CARD_EXP_DATE = new BankCardExpDate(TEST_MONTH, TEST_YEAR);
+    private static final BankCard TEST_BANK_CARD = new BankCard().setExpDate(TEST_BANK_CARD_EXP_DATE);
+    private static final CardDataProxyModel TEST_CARD_DATA_PROXY_MODEL = CardDataProxyModel.builder()
+            .expMonth(TEST_MONTH)
+            .expYear(TEST_YEAR)
+            .build();
+
+    @Test
+    public void bankCardDateTest() {
+        // exp date: year 2020, month 3
+
+        assertEquals("20200331", getFullCardExpDate(TEST_BANK_CARD_EXP_DATE));
+        assertEquals("20200331", getFullCardExpDate(TEST_CARD_DATA_PROXY_MODEL));
+
+        assertEquals(new Integer(31), getDayOfMonth(TEST_BANK_CARD_EXP_DATE));
+        assertEquals(new Integer(31), getDayOfMonth(TEST_CARD_DATA_PROXY_MODEL));
+        assertEquals(new Integer(30), getDayOfMonth(TEST_CARD_DATA));
+
+        assertEquals("20", BankCardUtils.getYearFromExpDate(TEST_BANK_CARD_EXP_DATE));
+        assertEquals("20", BankCardUtils.getYearFromExpDate(TEST_CARD_DATA_PROXY_MODEL));
+
+        assertEquals("20", getBankCardFormattedYear(TEST_YEAR));
+        assertEquals("03", getBankCardFormattedMonth(TEST_MONTH));
+
+        assertEquals("03", getMonthFromExpDate(TEST_BANK_CARD_EXP_DATE));
+        assertEquals("03", getMonthFromExpDate(TEST_CARD_DATA_PROXY_MODEL));
+
+        assertEquals("2003", expDateToString(new CardData(), TEST_BANK_CARD));
+        assertEquals("2006", expDateToString(TEST_CARD_DATA, TEST_BANK_CARD));
+
+        assertEquals("2003", expDateToString(TEST_CARD_DATA_PROXY_MODEL));
+        assertEquals("2003", expDateToString(TEST_EXP_DATE));
+        assertEquals("2003", expDateToString(TEST_BANK_CARD_EXP_DATE));
+
+        assertEquals("0620", expDateToString(TEST_YEAR, TEST_NEW_MONTH, MMYY_EXP_DATE_FORMAT));
+    }
+
+}
--- src/test/java/com/rbkmoney/adapter/common/utils/converter/PaymentDataConverterTest.java
+++ src/test/java/com/rbkmoney/adapter/common/utils/converter/PaymentDataConverterTest.java
@@ -3,8 +3,6 @@ package com.rbkmoney.adapter.common.utils.converter;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class PaymentDataConverterTest {
 
     @Test
