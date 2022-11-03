4955b62982718662bbfd5945350e4d4265af9bf5
Inal Arsanukaev <aiz@empayre.com>
2022-11-03 19:37:56 +0300

    Fix message

--- src/main/java/dev/vality/adapter/common/exception/HexDecodeException.java
+++ src/main/java/dev/vality/adapter/common/exception/HexDecodeException.java
@@ -2,6 +2,6 @@ package dev.vality.adapter.common.exception;
 
 public class HexDecodeException extends RuntimeException {
     public HexDecodeException(String message) {
-        super("Key must be in hex-format: " + message);
+        super("Secret must be in hex-format: " + message);
     }
 }
