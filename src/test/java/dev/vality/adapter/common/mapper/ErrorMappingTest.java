63a76cb0d4465d1fd685282a046c2e238c160149
Inal Arsanukaev <in.all.young@gmail.com>
2022-11-17 18:51:24 +0300

    Fix null descriptions (#41)
    * Fix null descriptions
    
    * Fix test
    
    Co-authored-by: Inal Arsanukaev <aiz@empayre.com>

--- src/test/java/dev/vality/adapter/common/mapper/ErrorMappingTest.java
+++ src/test/java/dev/vality/adapter/common/mapper/ErrorMappingTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ErrorMappingTest {
 
@@ -23,8 +22,27 @@ public class ErrorMappingTest {
         error1.setDescriptionRegex("Invalid Merchant Name");
         var errorMapping = new ErrorMapping("'%s' - '%s'", List.of(error, error1));
         assertThrows(WRuntimeException.class, () -> errorMapping.mapFailure("unknown"));
+        assertThrows(WRuntimeException.class, () -> errorMapping.mapFailure("00001"));
+
+        var error2 = new Error();
+        error2.setMapping("authorization_failed:insufficient_funds");
+        error2.setCodeRegex("00001");
+        var errorMappingFull = new ErrorMapping("'%s' - '%s'", List.of(error, error1, error2));
         assertEquals(
-                "Failure(code:authorization_failed, reason:'00001' - 'null', sub:SubFailure(code:unknown))",
-                errorMapping.mapFailure("00001").toString());
+                "Failure(code:authorization_failed, reason:'00001' - 'null', sub:SubFailure(code:insufficient_funds))",
+                errorMappingFull.mapFailure("00001").toString());
+    }
+
+    @Test
+    public void testNullDesc() {
+        Error error = new Error();
+        error.setCodeRegex("01");
+        error.setDescriptionRegex("desc");
+        error.setMapping("authorization_failed:unknown");
+        var errorMapping = new ErrorMapping("'%s' - '%s'", List.of(error));
+        assertThrows(WRuntimeException.class, () -> errorMapping.mapFailure("01"));
+
+        error.setDescriptionRegex(null);
+        assertNotNull(errorMapping.mapFailure("01"));
     }
 }
