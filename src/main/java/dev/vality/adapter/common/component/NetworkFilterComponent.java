dc7cdd30a642526fa8233a066ffa692e8cd7a56b
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2024-07-09 17:41:18 +0300

    [2.0.0] Bump the lib to Spring 3 (#66)

--- src/main/java/dev/vality/adapter/common/component/NetworkFilterComponent.java
+++ src/main/java/dev/vality/adapter/common/component/NetworkFilterComponent.java
@@ -4,11 +4,11 @@ import dev.vality.woody.api.flow.WFlow;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class NetworkFilterComponent {
