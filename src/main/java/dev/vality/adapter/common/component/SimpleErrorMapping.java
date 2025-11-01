8c411be62aa7adf9732f6c3bf23ebdbdd5a502ce
Anatoly Karlov <karleowne@gmail.com>
2025-11-01 17:10:02 +0700

    extract error-mapping module

--- src/main/java/dev/vality/adapter/common/component/SimpleErrorMapping.java
+++ src/main/java/dev/vality/adapter/common/component/SimpleErrorMapping.java
@@ -2,17 +2,12 @@ package dev.vality.adapter.common.component;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.vality.adapter.common.mapper.ErrorMapping;
-import lombok.RequiredArgsConstructor;
+import dev.vality.adapter.common.v2.mapper.ErrorMapping;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
-public class SimpleErrorMapping {
-
-    private final Resource filePath;
-    private final String patternReason;
+public record SimpleErrorMapping(Resource filePath, String patternReason) {
 
     public ErrorMapping createErrorMapping() throws IOException {
         var mapper = new ObjectMapper();
