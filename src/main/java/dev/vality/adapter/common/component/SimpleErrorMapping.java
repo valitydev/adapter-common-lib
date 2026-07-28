a4ddc0e93b05e4c6a903920abe159cb18c918a94
jvmdev4 <jvmdev4@gmail.com>
2026-07-28 10:09:11 +0400

    Bump spring boot 4
    Spring Boot 3.3.1 -> 4.0.6, Jackson 2 -> 3, versions from the BOM.
    Align woody 3.0.0 and damsel 1.698-99c91c9 with error-mapping-java-v2 1.0.0.
    
    Remove the vault/secret layer marked @Deprecated(forRemoval = true)

--- src/main/java/dev/vality/adapter/common/component/SimpleErrorMapping.java
+++ src/main/java/dev/vality/adapter/common/component/SimpleErrorMapping.java
@@ -1,17 +1,18 @@
 package dev.vality.adapter.common.component;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.vality.adapter.common.v2.mapper.ErrorMapping;
 import org.springframework.core.io.Resource;
+import tools.jackson.core.json.JsonReadFeature;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.io.IOException;
 
 public record SimpleErrorMapping(Resource filePath, String patternReason) {
 
     public ErrorMapping createErrorMapping() throws IOException {
-        var mapper = new ObjectMapper();
-        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        var mapper = JsonMapper.builder()
+                .enable(JsonReadFeature.ALLOW_SINGLE_QUOTES)
+                .build();
         return new ErrorMapping(filePath.getInputStream(), patternReason, mapper);
     }
 
